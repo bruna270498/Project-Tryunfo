ddefdaa37061a8a51a64b7995280bfc75eb5bd69
Bruna <brunaalmeida075@gmail.com>
2022-10-26 18:17:48 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -3,12 +3,54 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  state = {
+    cardName: '',
+    cardImage: '',
+    cardAttr1: '',
+    cardAttr2: 0,
+    cardAttr3: 0,
+    cardDescription: '',
+    cardRare: '',
+    cardTrunfo: false,
+    isSaveButtonDisabled: '',
+  };
+
+  onInputChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare, cardTrunfo, isSaveButtonDisabled } = this.state;
     return (
       <div>
         <h1>ADICIONE UMA NOVA CARTA</h1>
-        <Form />
-        <Card />
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onInputChange={ this.onInputChange }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+        />
       </div>
     );
   }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Card extends React.Component {
+  triunfo = (card) => ((card === true)
+    ? <span data-testid="trunfo-card">Super Trunfo</span> : null);
+
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2,
       cardAttr3, cardImage, cardRare, cardTrunfo } = this.props;
@@ -14,8 +17,7 @@ class Card extends React.Component {
         <h3 data-testid="attr2-card">{ cardAttr2 }</h3>
         <h3 data-testid="attr3-card">{ cardAttr3 }</h3>
         <span data-testid="rare-card">{ cardRare }</span>
-        {(cardTrunfo === true)
-          ? <span data-testid="trunfo-card">{ cardTrunfo }</span> : null }
+        { this.triunfo(cardTrunfo) }
       </div>
     );
   }
